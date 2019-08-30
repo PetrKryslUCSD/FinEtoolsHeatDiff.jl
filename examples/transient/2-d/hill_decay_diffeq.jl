a5ad4cfb579aba2f5b5bdc00115f8fa3ddfcbb83
Petr Krysl <pkrysl@ucsd.edu>
2019-08-30 08:48:52 +0200

    cleaned up

--- examples/transient/2-d/hill_decay_diffeq.jl
+++ examples/transient/2-d/hill_decay_diffeq.jl
@@ -6,21 +6,6 @@ import LinearAlgebra: lu!
 using UnicodePlots
 using DifferentialEquations
 
-# mutable struct LinSolveFactorize{F}
-#   factorization::F
-#   A
-# end
-# function (p::LinSolveFactorize)(x,A,b,matrix_updated=false)
-#   if matrix_updated
-#     p.A = p.factorization(A)
-#   end
-#   x .= p.A\b
-# end
-# function (p::LinSolveFactorize)(::Type{Val{:init}},f,u0_prototype)
-#   LinSolveFactorize(p.factorization,nothing)
-# end
-# LinSolveFactorize(factorization) = LinSolveFactorize(factorization,nothing)
-
 function hill_decay_t3()
 	thermal_conductivity =  [i==j ? 0.2 : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
 	Width = 60.0
@@ -28,7 +13,7 @@ function hill_decay_t3()
 	N = 10
 	specific_heat = 1.0
 	dt = 2.0; # time step
-	tend = 200*dt; # length of the time interval
+	tend = 20*dt; # length of the time interval
 	T0(x, y) = 500.0/(x^2+y^2+5);# Initial distribution of temperature
 	
 	tolerance =Width/N/100;
@@ -61,37 +46,25 @@ function hill_decay_t3()
     
     Tn = gathersysvec(Temp)
     
-	# f(dT, T, p, t) = begin dT .= (A * T); end
-	# tspan = (0.0, tend)
-	
-	# linsolve = LinSolveFactorize(lu!)
-	
-	# odeff = ODEFunction(f)
-	# prob = ODEProblem(odeff, Tn, tspan)
-	# sol = solve(prob, Rosenbrock23(linsolve=linsolve))
-	# @show sol
-
 	f(dT, T, p, t) = begin dT .= -(C \ (K * T)); end
 	tspan = (0.0, tend)
 	prob = ODEProblem(f, Tn, tspan)
-	# sol = solve(prob, Rosenbrock23(autodiff = false))
 	sol = solve(prob, ImplicitEuler(autodiff = false))
 	# @show sol
 
+	# This is postprocessing  to extract the data for the plot.
 	ts = Float64[]
 	Corner_T = Float64[];
-	for t in 0.0:dt:tend 
+	for t in sol.t
 		T = sol(t)
 		push!(ts, t)
 		push!(Corner_T, T[cornerdof][1])
 	end
 	
-	@show Corner_T
+	# @show Corner_T
 	@show minimum(Corner_T), maximum(Corner_T)
 	plt = lineplot(vec(ts), vec(Corner_T), canvas = DotCanvas, title = "Transient temperature at the corner", name = "T", xlabel = "Time", ylabel = "T")
 	display(plt)
-	# File =  "a.vtk"
-	# MeshExportModule.vtkexportmesh (File, fes.conn, [geom.values Temp.values], MeshExportModule.T3; scalars=Temp.values, scalars_name ="Temperature")
 
     true
 end
